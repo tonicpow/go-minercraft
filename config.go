3f2850605fe4dce8bc5490c1a9ca4185049f32cb
mrz1836 <mrz1818@gmail.com>
2021-04-09 10:33:12 -0400

    Bumping version

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package minercraft
 const (
 
 	// version is the current package version
-	version = "v0.2.3"
+	version = "v0.2.4"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-minercraft: " + version
