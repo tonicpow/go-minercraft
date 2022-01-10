faf0e78dfcd766c01d4ea6e0e369f8fff7e16e3f
mrz1836 <mrz1818@gmail.com>
2022-01-10 16:21:17 -0500

    Added policy quote request

---
+++ examples/policy_quote/policy_quote.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/tonicpow/go-minercraft"
+)
+
+func main() {
+
+	// Create a new client
+	client, err := minercraft.NewClient(nil, nil, nil)
+	if err != nil {
+		log.Fatalf("error occurred: %s", err.Error())
+	}
+
+	// Select the miner
+	miner := client.MinerByName(minercraft.MinerTaal)
+
+	// Get a policy quote from a miner
+	var response *minercraft.PolicyQuoteResponse
+	if response, err = client.PolicyQuote(context.Background(), miner); err != nil {
+		log.Fatalf("error occurred: %s", err.Error())
+	}
+
+	// Display the results
+	// todo: At this time (1/10/21) there is no policy response from any miner
+	log.Printf("miner: %s", response.Miner.Name)
+	log.Printf("callbacks: %+v", response.Quote.Callbacks)
+	log.Printf("policy quote: %+v", response.Quote.Policies)
+}
