f484e75a207372e14edf2126e257c2cb859b06ff
mrz1836 <mrz1818@gmail.com>
2023-06-12 19:00:10 -0400

    Last version for 1.0

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ import "time"
 const (
 
 	// version is the current package version
-	version = "v0.9.1"
+	version = "v0.9.2"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-minercraft: " + version
