57dffde3ecc76fd4e5a8a9a727b61a2723e427b2
mrz1836 <mrz1818@gmail.com>
2024-05-03 16:13:23 -0400

    Bump version

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ import "time"
 const (
 
 	// version is the current package version
-	version = "v2.0.8"
+	version = "v2.1.0"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-minercraft: " + version
